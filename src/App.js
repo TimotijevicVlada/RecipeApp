db1e8168ca4b9950aee41273ade2b605fc3b325d
Vladimir <timotijevicvlada@gmail.com>
2021-10-17 23:54:27 +0200

    Start to work on recipe app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react";
 import './App.css';
 
 function App() {
+
+  const API_ID = "a6623b54";
+  const API_KEY = "0c2cb1931ef23530653b88020c54e432";
+
+
+  const fetchFood = async () => {
+    try {
+      const response = await fetch(`https://api.edamam.com/search?&app_id=${API_ID}&app_key=${API_KEY}&q=pizza`);
+      const data = await response.json();
+      console.log(data);
+    } catch(err) {
+      console.log(err);
+    }
+    
+  }
+
+  useEffect(() => {
+    fetchFood();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pozdrav react</h1>
     </div>
   );
 }
